1c74b1afcf69ef28d71e4008106d2e0aadae638e
Guillaume <g.h01@hotmail.com>
2022-03-27 19:20:36 +0000

    fix dispatcher

--- lib/Controller/GroupChangeController.php
+++ lib/Controller/GroupChangeController.php
@@ -11,7 +11,7 @@ use OCP\Group\Events\UserRemovedEvent;
 use OCP\IGroupManager;
 use OCP\IRequest;
 use OCP\IUserManager;
-use Symfony\Component\EventDispatcher\EventDispatcher;
+use OCP\EventDispatcher\IEventDispatcher;
 
 class GroupChangeController extends Controller {
 
@@ -21,34 +21,39 @@ class GroupChangeController extends Controller {
 
 	private $groupManager;
 
-	public function __construct(string $appName, IRequest $request, EventDispatcher $eventDispatcher, IUserManager $userManager, IGroupManager $groupManager) {
+	public function __construct(string $appName, IRequest $request, IEventDispatcher $eventDispatcher, IUserManager $userManager, IGroupManager $groupManager) {
 		$this->dispatcher = $eventDispatcher;
 		$this->userManager = $userManager;
 		$this->groupManager = $groupManager;
 		parent::__construct($appName, $request);
 	}
 
+	/**
+	 * @NoCSRFRequired
+	 */
 	public function syncGroup(string $username, string $groupname, string $operation) {
 
-		$user = $this->userManager->get($username);
-		$group = $this->groupManager->get($groupname);
+		$ud = base64_decode($username);
+		$gd = base64_decode($groupname);
+		$user = $this->userManager->get($ud);
+		$group = $this->groupManager->get($gd);
 
 		if ($user && $group) {
 
 			$done = false;
-			if ($operation === 'join' && $group->inGroup($user)) {
-				$this->dispatcher->dispatch(new UserAddedEvent($group, $user));
+			if ($operation === 'join') {
+				$this->dispatcher->dispatchTyped(new UserAddedEvent($group, $user));
 				$done = true;
-			} else if ($operation === 'leave' && !$group->inGroup($user)) {
-				$this->dispatcher->dispatch(new UserRemovedEvent($group, $user));
+			} else if ($operation === 'leave') {
+				$this->dispatcher->dispatchTyped(new UserRemovedEvent($group, $user));
 				$done = true;
 			}
 
 			return new JSONResponse(
 				[
 					'operation' => $operation,
-					'userid' => $username,
-					'groupid' => $groupname,
+					'userid' => $ud,
+					'groupid' => $gd,
 					'ok' => $done,
 					'error' => [],
 				]
@@ -58,8 +63,8 @@ class GroupChangeController extends Controller {
 		return new JSONResponse(
 			[
 				'operation' => $operation,
-				'userid' => $username,
-				'groupid' => $groupname,
+				'userid' => $ud,
+				'groupid' => $gd,
 				'ok' => false,
 				'error' => [
 					'user_found' => $user !== null,
